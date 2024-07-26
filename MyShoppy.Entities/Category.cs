c6d7d85e3128663c0325e120e5176086a5cb61db
Filobateer Essam <108236186+FilobateerEssam@users.noreply.github.com>
2024-07-26 23:50:00 +0300

    Finished CRUD Category

--- MyShoppy.Entities/Category.cs
+++ MyShoppy.Entities/Category.cs
@@ -15,7 +15,7 @@ namespace MyShoppy.Entities
         public string Name { get; set; }
 
 
-        public string Description { get; set; }
+        public string? Description { get; set; }
 
         public DateTime CreatedTime { get; set; } = DateTime.Now;
 
--- MyShoppy.Web/Controllers/CategoryController.cs
+++ MyShoppy.Web/Controllers/CategoryController.cs
@@ -12,6 +12,7 @@ namespace MyShoppy.Web.Controllers
         {
             _applicationContext = applicationContext;
         }
+
         public IActionResult Index()
         {
             IEnumerable<Category> CategoriesList = _applicationContext.Categories.ToList();
@@ -25,7 +26,6 @@ namespace MyShoppy.Web.Controllers
             return View();
         }
 
-
         [HttpPost]
         [ValidateAntiForgeryToken] //Protect Web From Cross Side Forgery Attacks
         public IActionResult Create(Category category)
@@ -33,6 +33,10 @@ namespace MyShoppy.Web.Controllers
             if (category.Name == "Filo")
             {
                 ModelState.AddModelError("Name", "Filo is not allowed");
+            }
+            if (category.Description == null)
+            {
+                category.Description = " ";
             }
             if (ModelState.IsValid)
             {
@@ -43,6 +47,80 @@ namespace MyShoppy.Web.Controllers
             return View();
         }
 
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            // Check ID
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
 
+            // Get Category By ID
+
+            var categoryfromDb = _applicationContext.Categories.Find(id);
+
+            return View(categoryfromDb);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken] //Protect Web From Cross Side Forgery Attacks
+        public IActionResult Edit(Category category)
+        {
+            if (category.Description == null)
+            {
+                category.Description = " ";
+            }
+
+            if (ModelState.IsValid)
+            {
+                _applicationContext.Categories.Update(category);
+                _applicationContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            // Check ID
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            // Get Category By ID
+
+            var categoryfromDb = _applicationContext.Categories.Find(id);
+
+            return View(categoryfromDb);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken] //Protect Web From Cross Side Forgery Attacks
+        public IActionResult DeleteCategory(int? id)
+        {
+
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var categoryFromDb = _applicationContext.Categories.Find(id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _applicationContext.Categories.Remove(categoryFromDb);
+                _applicationContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
